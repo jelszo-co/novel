1756608a1528423c42295bff7e3002f8507f6df2
tasztalos69 <tasztalos69@gmail.com>
2020-03-25 20:12:10 +0100

    Main markup & add tests for router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './css/app.scss';
 
 import Landing from './pages/Landing';
+import List from './pages/List';
+import err404 from './pages/404';
 
 const App = () => (
-  <Router data-test='component-app'>
+  <Router data-test='router'>
     <Switch>
+      <Route exact path='/list' component={List} />
       <Route exact path='/' component={Landing} />
+      <Route component={err404} />
     </Switch>
   </Router>
 );
