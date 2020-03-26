660f3879a8e0cd8440b17964f3e01761b7964b76
tasztalos69 <tasztalos69@gmail.com>
2020-03-26 11:34:44 +0100

    Add linter error

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const err404 = () => (
-  <div>
-    <h1>404</h1>
-  </div>
-);
+const err404 = () => {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  );
+};
 
 export default err404;
