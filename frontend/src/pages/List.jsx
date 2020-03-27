242dded36d27e48954392b8ecca84b812d8d6db3
tasztalos69 <tasztalos69@gmail.com>
2020-03-27 19:51:19 +0100

    Search highlighting

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 import Title from './components/Title';
 import Menu from './components/Menu';
+
+import { ReactComponent as Search } from '../assets/search.svg';
+
+import '../css/all/list.scss';
 
 class List extends Component {
   constructor(props) {
@@ -9,28 +16,31 @@ class List extends Component {
     this.state = {
       novels: {
         HU: {
-          '2020': [
+          2020: [
             {
+              id: 1,
               title: 'Első',
               path: 'elso',
               lore: 'Az én els novellám.',
-              'uploaded-at': '2020-01-01T12:00:00.000Z',
+              uploadedAt: '2020-01-01T12:00:00.000Z',
             },
 
             {
+              id: 2,
               title: 'Második',
-              path: 'elso',
+              path: 'elso-novellam',
               lore: 'Az én els novellám.',
-              'uploaded-at': '2020-01-01T12:00:00.000Z',
+              uploadedAt: '2020-01-01T12:00:00.000Z',
             },
           ],
 
-          '2021': [
+          2021: [
             {
+              id: 3,
               title: 'Harmadik',
-              path: 'elso',
+              path: 'elso-harmadik',
               lore: 'Az én els novellám.',
-              'uploaded-at': '2021-01-01T12:00:00.000Z',
+              uploadedAt: '2021-01-01T12:00:00.000Z',
             },
           ],
         },
@@ -39,23 +49,49 @@ class List extends Component {
       },
       param: '',
     };
+    const { novels } = this.state;
     this.state = {
       ...this.state,
-      listMode: Object.keys(this.state.novels.EN).length > 0 ? 'dual' : 'only',
+      listMode: Object.keys(novels.EN).length > 0 ? 'dual' : 'only',
     };
   }
 
   render() {
     const { listMode, param, novels } = this.state;
     const listNovels = inp => {
-      const patt = new RegExp(`${param}`, 'gi');
+      const patt = new RegExp(`(${param.trim()})`, 'gi');
+      const highlight = text => {
+        const parts = text.split(patt);
+        return parts.map((part, i) => (
+          <span
+            key={i}
+            className={
+              part.toLowerCase() === param.toLowerCase() ? 'matched' : ''
+            }
+          >
+            {part}
+          </span>
+        ));
+      };
       return Object.entries(inp).map(yr =>
         yr[1]
-          .filter(
-            ({ title, path, lore }) =>
-              title.match(patt) || lore.match(patt) || path.match(patt),
-          )
-          .map(novel => novel.title),
+          .filter(({ title, lore }) => title.match(patt) || lore.match(patt))
+          .map(({ id, title, lore, path, uploadedAt }) => {
+            return (
+              <div className='novel-card' key={id}>
+                <Link to={`/novels/${path}`}>
+                  <h3 className='novel-title'>
+                    {param ? highlight(title) : title}
+                  </h3>
+                  <p className='novel-lore'>{param ? highlight(lore) : lore}</p>
+                </Link>
+                <div className='date-cont'></div>
+                <Moment format='MMM'>{uploadedAt}</Moment>
+                <br />
+                <Moment format='D'>{uploadedAt}</Moment>
+              </div>
+            );
+          }),
       );
     };
 
@@ -68,9 +104,11 @@ class List extends Component {
           <input
             type='text'
             name='search'
-            id='search'
+            id='search-bar'
+            placeholder={t('search')}
             onChange={e => this.setState({ param: e.target.value })}
           />
+          <Search className='search-icon' />
         </div>
         <div id='hu' className={`${listMode}`}>
           {listNovels(novels.HU)}
@@ -84,5 +122,9 @@ class List extends Component {
     );
   }
 }
+
+List.propTypes = {
+  t: PropTypes.func.isRequired,
+};
 
 export default withTranslation()(List);
--- frontend/src/pages/components/Title.jsx
+++ frontend/src/pages/components/Title.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import '../../css/components/title.scss';
 
-const Title = props => <h2 className='title'>{props.children}</h2>;
+const Title = ({ children }) => <h2 className='title'>{children}</h2>;
+
+Title.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default Title;
