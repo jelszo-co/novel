e9eda40f5dad54d4b4b493c7a722de4444f6baab
Bence <btoth0211@gmail.com>
2020-03-28 18:44:31 +0100

    static url

--- backend/novel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from novel.views import GetAllNovels, NovelTools
-
-urlpatterns = [
-    path('', GetAllNovels.as_view()),
-    path('<str:path>',NovelTools.as_view())
-]
