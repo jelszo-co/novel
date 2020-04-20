87ef5ee77b107c76c4c8393ef40ce0c0bf2c74ed
tasztalos69 <tasztalos69@gmail.com>
2020-04-20 15:15:35 +0200

    Change response data processing

--- frontend/src/pages/Novel.jsx
+++ frontend/src/pages/Novel.jsx
@@ -55,7 +55,7 @@ const Novel = ({ match, user: { role } }) => {
         const res = await axios.post(
           `${process.env.REACT_APP_SRV_ADDR}/novel/${match.params.title}/favorite`,
         );
-        setNovel({ ...novel, favorite: res.data });
+        setNovel({ ...novel, favorite: res.data.favorite });
       } catch (err) {
         console.error(err);
         setPopup('Hiba!', 'err');
