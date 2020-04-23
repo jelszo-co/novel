1b1a7058c02e0d3a8a9dd41292c8acf1ee6d7429
Bence <btoth0211@gmail.com>
2020-04-23 22:10:00 +0200

    created docker config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
-
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-    python3 manage.py migrate
-    python3 manage.py collectstatic --no-input --clear
-fi
-
+python3 manage.py migrate
+envsubst < /etc/nginx/conf.d/novel.conf.temp > /etc/nginx/conf.d/novel.conf
+nginx
+# gunicorn backend.wsgi
 exec "$@"
