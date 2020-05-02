3e86337c6a4b43f1771a99337fefc0ae01757c96
Bence <btoth0211@gmail.com>
2020-05-02 10:45:34 +0200

    404

--- backend/backend/frontend_view.py
+++ backend/backend/frontend_view.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 
 def frontend_view(request, *args, **kwargs):  # pragma: no cover
-    return render(request, 'index.html')
+    path = request.path.split("/")
+    if len(path) >= 2 and path[1] == "api":
+        return JsonResponse({"error": "Endpoint not found"})
+    else:
+        return HttpResponse()  # render(request, 'index.html')
