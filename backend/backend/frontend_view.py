faf16b9578b337ba350f8e3da04df8fb8b1dbaa1
Bence <btoth0211@gmail.com>
2020-05-02 11:05:55 +0200

    heroku clone

--- backend/backend/frontend_view.py
+++ backend/backend/frontend_view.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse, JsonResponse
 def frontend_view(request, *args, **kwargs):  # pragma: no cover
     path = request.path.split("/")
     if len(path) >= 2 and path[1] == "api":
-        return JsonResponse({"error": "Endpoint not found"})
+        return JsonResponse({"error": "Endpoint not found"}, status=404)
     else:
         return HttpResponse()  # render(request, 'index.html')
