ff213735a84ac698401f3bd40ea9813f6e1b6a21
Bence <btoth0211@gmail.com>
2020-05-29 19:28:26 +0200

    get comment by path, send comment

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -7,7 +7,8 @@ from novel.models import Novel
 
 class Comment(models.Model):
     sender = models.ForeignKey(User, on_delete=models.SET("[törölt]"))
+    recipient = models.ForeignKey(User, on_delete=models.SET("[törölt]"), related_name="recipient", null=True)
     novel = models.ForeignKey(Novel, models.CASCADE)
     content = models.TextField()
     writtenAt = models.DateTimeField(auto_now_add=True)
-    parentComment = models.ForeignKey('self', on_delete=models.CASCADE)
+    parentComment = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from comment.views import CommentByPath
+
+urlpatterns = [
+    path('path/<str:path>', CommentByPath.as_view())
 ]
